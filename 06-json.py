0f85364ef6674312263a5d4084e9ea9091e485c8
Alex-GCX <41978022+Alex-GCX@users.noreply.github.com>
2020-08-05 12:59:53 +0800

    Update 06-json.py

--- 06-json.py
+++ 06-json.py
@@ -10,12 +10,17 @@ def show_json():
         'age': '12'
     }
     # 1.使用json模块
-    # json.dump(字典) 将dict转换为json字符串
+    # json.dumps(字典) 将dict转换为json字符串
     # json.loads(字符串) 将字符串转换为dict
+    # json_str = json.dumps(data)
+    # return json_str, 200, {"Content-Type": "application/json"}
 
     # 2.使用jsonify
     # jsonify可以将dict转换为字符串并返回一个response对象,
     # 并且设置响应头的 Content-Type 为application/json
-    print(type(Response('aaaaa')))
-    print(type(jsonify(data)))
+    print(type(Response('aaaaa')))  # <class 'flask.wrappers.Response'>
+    print(type(jsonify(data)))  # <class 'flask.wrappers.Response'>
+    # 方式一
     return jsonify(data)
+    # 方式二
+    # return jsonify(city="shanghai", country="CN")
